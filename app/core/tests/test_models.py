8288ba9fdf38a5190e7ac5ac9b5098d2d087cb38
stefano capra <ste.capra95@gmail.com>
2020-04-06 09:34:20 +0200

    Add user functionality

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_succesfull(self):
+        """Test creating a new user with an email is succesfull"""
+        email = 'spruzzi23@gmail.com'
+        password = '123123'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normilize(self):
+        email = 'spruzzi23@GMAIL.COM'
+        password = '123123'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+
+    def test_email_validation(self):
+        with self.assertRaises(ValueError):
+            user = get_user_model().objects.create_user(
+                None,
+                '123123'
+            )
+
+
+    def test_create_new_useruser(self):
+        user = get_user_model().objects.create_superuser(
+            'spruzzi23@gmail.com',
+            '123123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+
+
