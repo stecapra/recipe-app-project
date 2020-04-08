45e8483363f95b70d004c86fd1d3b16ae5d3e7c1
stefano capra <ste.capra95@gmail.com>
2020-04-08 15:52:25 +0200

    Add tag endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 										PermissionsMixin
 
 
-class UserManager(BaseUserManager):
+from django.conf import settings
 
+
+class UserManager(BaseUserManager):
 	def create_user(self, email, password=None, **extra_fields):
 		if not email:
 			raise ValueError('Email is mandatory')
@@ -31,6 +33,17 @@ class User(AbstractBaseUser, PermissionsMixin):
 	objects = UserManager()
 
 	USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=255)
+	user = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete = models.CASCADE
+	)
+
+	def __str__(self):
+		return self.name
 
 
 
